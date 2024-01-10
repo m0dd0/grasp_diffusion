978d625e2b5b3cc5af24bf35f084495260bc85d8
Moritz Hesche <mo.hesche@gmail.com>
2024-01-10 18:47:50 +0100

    some experimetsn with object scling

--- scripts/sample/generate_pointcloud_6d_grasp_poses.py
+++ scripts/sample/generate_pointcloud_6d_grasp_poses.py
@@ -5,42 +5,47 @@
 # 'Sheep', 'Shower', 'Sink', 'SoapBottle', 'SodaCan','Spoon', 'Statue', 'Teacup', 'Teapot', 'ToiletPaper',
 # 'ToyFigure', 'Wallet','WineGlass','Cow', 'Sheep', 'Cat', 'Dog', 'Pizza', 'Elephant', 'Donkey', 'RubiksCube', 'Tank', 'Truck', 'USBStick']
 
+
 def parse_args():
     p = configargparse.ArgumentParser()
-    p.add('-c', '--config_filepath', required=False, is_config_file=True, help='Path to config file.')
+    p.add(
+        "-c",
+        "--config_filepath",
+        required=False,
+        is_config_file=True,
+        help="Path to config file.",
+    )
 
-    p.add_argument('--obj_id', type=int, default=0)
-    p.add_argument('--n_grasps', type=str, default='200')
-    p.add_argument('--obj_class', type=str, default='Laptop')
-    p.add_argument('--device', type=str, default='cuda:0')
-    p.add_argument('--eval_sim', type=bool, default=False)
-    p.add_argument('--model', type=str, default='grasp_dif_multi')
-
+    p.add_argument("--obj_id", type=int, default=0)
+    p.add_argument("--n_grasps", type=str, default="200")
+    p.add_argument("--obj_class", type=str, default="Laptop")
+    p.add_argument("--device", type=str, default="cuda:0")
+    p.add_argument("--eval_sim", type=bool, default=False)
+    p.add_argument("--model", type=str, default="grasp_dif_multi")
 
     opt = p.parse_args()
     return opt
 
 
-def get_approximated_grasp_diffusion_field(p, args, device='cpu'):
+def get_approximated_grasp_diffusion_field(p, args, device="cpu"):
     model_params = args.model
     batch = 10
     ## Load model
-    model_args = {
-        'device': device,
-        'pretrained_model': model_params
-    }
+    model_args = {"device": device, "pretrained_model": model_params}
     model = load_model(model_args)
 
-    context = to_torch(p[None,...], device)
+    context = to_torch(p[None, ...], device)
     model.set_latent(context, batch=batch)
 
     ########### 2. SET SAMPLING METHOD #############
-    generator = Grasp_AnnealedLD(model, batch=batch, T=70, T_fit=50, k_steps=2, device=device)
+    generator = Grasp_AnnealedLD(
+        model, batch=batch, T=70, T_fit=50, k_steps=2, device=device
+    )
 
     return generator, model
 
 
-def sample_pointcloud(obj_id=0, obj_class='Mug'):
+def sample_pointcloud(obj_id=0, obj_class="Mug"):
     acronym_grasps = AcronymGraspsDirectory(data_type=obj_class)
     mesh = acronym_grasps.avail_obj[obj_id].load_mesh()
 
@@ -50,33 +55,32 @@ def sample_pointcloud(obj_id=0, obj_class='Mug'):
     rot = sampled_rot.as_matrix()
     rot_quat = sampled_rot.as_quat()
 
-    P = np.einsum('mn,bn->bm', rot, P)
-    P *= 8.
+    P = np.einsum("mn,bn->bm", rot, P)
+    P *= 8.0
     P_mean = np.mean(P, 0)
     P += -P_mean
 
     H = np.eye(4)
-    H[:3,:3] = rot
+    H[:3, :3] = rot
     mesh.apply_transform(H)
-    mesh.apply_scale(8.)
+    mesh.apply_scale(8.0)
     H = np.eye(4)
-    H[:3,-1] = -P_mean
+    H[:3, -1] = -P_mean
     mesh.apply_transform(H)
     translational_shift = copy.deepcopy(H)
-
-
 
     return P, mesh, translational_shift, rot_quat
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import copy
     import configargparse
+
     args = parse_args()
 
     EVAL_SIMULATION = args.eval_sim
     # isaac gym has to be imported here as it is supposed to be imported before torch
-    if (EVAL_SIMULATION):
+    if EVAL_SIMULATION:
         # Alternatively: Evaluate Grasps in Simulation:
         from isaac_evaluation.grasp_quality_evaluation import GraspSuccessEvaluator
 
@@ -89,12 +93,10 @@ if __name__ == '__main__':
 
     import torch
 
-
-
-    print('##########################################################')
-    print('Object Class: {}'.format(args.obj_class))
+    print("##########################################################")
+    print("Object Class: {}".format(args.obj_class))
     print(args.obj_id)
-    print('##########################################################')
+    print("##########################################################")
 
     n_grasps = int(args.n_grasps)
     obj_id = int(args.obj_id)
@@ -110,22 +112,31 @@ if __name__ == '__main__':
 
     H_grasp = copy.deepcopy(H)
     # counteract the translational shift of the pointcloud (as the spawned model in simulation will still have it)
-    H_grasp[:, :3, -1] = (H_grasp[:, :3, -1] - torch.as_tensor(trans[:3,-1],device=device)).float()
-    H[..., :3, -1] *=1/8.
-    H_grasp[..., :3, -1] *=1/8.
+    H_grasp[:, :3, -1] = (
+        H_grasp[:, :3, -1] - torch.as_tensor(trans[:3, -1], device=device)
+    ).float()
+    H[..., :3, -1] *= 1 / 8.0
+    H_grasp[..., :3, -1] *= 1 / 8.0
 
     ## Visualize results ##
     from se3dif.visualization import grasp_visualization
 
     vis_H = H.squeeze()
-    P *=1/8
-    mesh = mesh.apply_scale(1/8)
+    P *= 1 / 8
+    mesh = mesh.apply_scale(1 / 8)
     grasp_visualization.visualize_grasps(to_numpy(H), p_cloud=P, mesh=mesh)
 
-    if (EVAL_SIMULATION):
+    if EVAL_SIMULATION:
         ## Evaluate Grasps in Simulation##
         num_eval_envs = 10
-        evaluator = GraspSuccessEvaluator(obj_class, n_envs=num_eval_envs, idxs=[args.obj_id] * num_eval_envs, viewer=True, device=device, \
-                                          rotations=[rot_quad]*num_eval_envs, enable_rel_trafo=False)
+        evaluator = GraspSuccessEvaluator(
+            obj_class,
+            n_envs=num_eval_envs,
+            idxs=[args.obj_id] * num_eval_envs,
+            viewer=True,
+            device=device,
+            rotations=[rot_quad] * num_eval_envs,
+            enable_rel_trafo=False,
+        )
         succes_rate = evaluator.eval_set_of_grasps(H_grasp)
-        print('Success cases : {}'.format(succes_rate))
+        print("Success cases : {}".format(succes_rate))
