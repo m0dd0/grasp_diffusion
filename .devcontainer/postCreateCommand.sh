d7cc696048d88e01643335c4037f6bd341a20ef3
Moritz Hesche <mo.hesche@gmail.com>
2024-01-16 10:59:20 +0100

    updated devcontainer setup

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,1 +1,6 @@
-pip install 
+# cd /home/catkin_ws 
+# rosdep update 
+# rosdep install --from-paths src --ignore-src -y -r 
+# catkin config --extend /opt/ros/noetic 
+# catkin build 
+# python3 -m pip install -e /home/catkin_ws/src/grasping-benchmark-panda[se3dif]
